f0c547d6aaac044abc8646659bbf2629804cb657
GABHI <80673476+Ganeshnani-web@users.noreply.github.com>
2021-04-02 07:30:19 -0700

    first coommut

---
+++ ineedtodo/src/test/java/com/anytime/netdo/dosomething.java
@@ -0,0 +1,19 @@
+package com.anytime.netdo;
+
+import org.testng.annotations.Test;
+
+public class dosomething 
+{
+	@Test
+	public void dosm()
+	{
+	String un = System.getProperty("username");
+	String pwd = System.getProperty("password");
+	
+	System.out.println("username is"+ un);
+	System.out.println("username is"+ pwd);
+	
+	
+	}
+	
+}
